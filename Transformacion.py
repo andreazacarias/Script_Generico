9154533fd48cf800557f4ce5bc542c5e24f0ffdf
andreazacarias <102975061+andreazacarias@users.noreply.github.com>
2022-04-11 10:00:13 -0400

    Update Transformacion.py
    Estoy completando el documento, agregándole definiciones.

--- Transformacion.py
+++ Transformacion.py
@@ -1,18 +1,18 @@
 # Importar paquetes
 
-import pandas as pd # paquete para...
+import pandas as pd # paquete para manejar datos
 from pandas_profiling import ProfileReport
-import numpy as np
-import matplotlib.pyplot as plt
-%matplotlib inline
-import janitor
+import numpy as np # paquete para manejar datos
+import matplotlib.pyplot as plt # paquete para graficar
+%matplotlib inline # para graficar
+import janitor # para limpiar los nombres de la base de datos
 import seaborn as sns
 import lifelines
 import statistics
 from sklearn.impute import SimpleImputer
-from lifelines import KaplanMeierFitter, CoxPHFitter
-from lifelines.statistics import logrank_test
+from lifelines import KaplanMeierFitter, CoxPHFitter # Paquete para modelos de sobrevida
+from lifelines.statistics import logrank_test # paquete para modelos de sobrevida
 from scipy import stats
-from datetime import date
+from datetime import date # paquete para manejar formatos fecha
 
 # Ingesta de Datos
